9ce0cafca9658c8c637eeaee7b724849d2ca988d
ZeM4artins <a97903@alunos.uminho.pt>
2022-05-27 00:08:01 +0100

    some changes

--- src/sdstore.c
+++ src/sdstore.c
@@ -1,9 +1,3 @@
-// #include <unistd.h>
-// #include <fcntl.h>
-// #include <string.h>
-// #include <stdio.h>
-
-
 // int main (int argc, char argv[]) {
 //     char buffer[4000];
 
@@ -28,20 +22,53 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <string.h>
+#include <stdlib.h>
 
-int main(int argc, char * argv[]) {
-    char buf[1024];
-    int bytes_read;
+// int main(int argc, char * argv[]) {
+//     char buf[1024];
+//     int bytes_read;
     
-    if(mkfifo("fifo", 0777)<0) perror("mkfifo");
+//     if(mkfifo("fifo", 0777)<0) perror("mkfifo");
 
-    int fd_npipe = open("fifo", O_WRONLY); //cliente escreve para o pipe
+//     int fd_npipe = open("fifo", O_WRONLY); //cliente escreve para o pipe
 
-    while((bytes_read=read(0, buf, 1024))>0) {
-        write(fd_npipe, buf, bytes_read);
+//     while((bytes_read=read(0, buf, 1024))>0) {
+//         write(fd_npipe, buf, bytes_read);
+//     }
+    
+//     close(fd_npipe);
+
+//     return 0;
+// }
+
+int main(int argc, char * argv[])
+{
+    int fd;
+
+    char * myfifo = "/tmp/myfifo";
+
+
+    mkfifo(myfifo, 0666);
+
+    while (1)
+    {
+        fd = open(myfifo, O_WRONLY);
+
+
+        if (write(fd, argv+3, sizeof(char) * argc) < 0) {
+            //code
+            // STATUS ?? 
+        }        
+        close(fd); // fifo le as infos
+
+
+
+        //... ... ... samples/file-a outputs/file-a-output bcompress nop gcompress encrypt nop
     }
-    
-    close(fd_npipe);
 
     return 0;
 }
+
+                
+
--- src/sdstored.c
+++ src/sdstored.c
@@ -1,43 +1,84 @@
-// #include <unistd.h>
-// #include <fcntl.h>
-// #include <sys/wait.h>
-// #include <sys/stat.h>
-// #include <sys/types.h>
-// #include <string.h>
-// #include <stdlib.h>
-// #include <stdio.h>
-// #include <signal.h>
-
-// int main(int argc, char *argv[]){
-//     mkfifo("fifo_cl-sv", 0644);
-//     mkfifo("fifo_sv-cl", 0644);
-    
-
-    
-
-// }
-
-
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
 
-int main(int argc, char * argv[]) {
-    char buf[1024];
-    int bytes_read;
+
+// int main(int argc, char * argv[]) {
+//     char buf[1024];
+//     int bytes_read;
     
-    if(mkfifo("fifo", 0777)<0) perror("mkfifo");
+//     if(mkfifo("fifo", 0777)<0) perror("mkfifo");
 
-    int fd_log = open("log.txt", O_CREAT | O_TRUNC | O_WRONLY, 0600);
-    int fd_npipe = open("fifo", O_RDONLY); //server lê do pipe
+//     int fd_log = open("log.txt", O_CREAT | O_TRUNC | O_WRONLY, 0600);
+//     int fd_npipe = open("fifo", O_RDONLY); //server lê do pipe
 
-    while((bytes_read=read(fd_npipe, buf, 1024))>0) {
-        write(fd_log, buf, bytes_read);
+//     while((bytes_read=read(fd_npipe, buf, 1024))>0) {
+//         write(fd_log, buf, bytes_read);
+//     }
+//     close(fd_log);
+//     close(fd_npipe);
+
+//     return 0;
+// }
+
+#define nop 1
+#define bcompress 2
+#define bdecompress 3
+#define gcompress 4
+#define gdecompress 5
+#define encrypt 2
+#define decrypt 3
+
+
+
+int main(int argc, char * argv[])
+{
+    int fd1;
+    int i = 0;
+    char *source = malloc(sizeof(char) * 100);
+    char *destiny = malloc(sizeof(char) * 100);
+
+    char * myfifo = "/tmp/myfifo";
+
+    mkfifo(myfifo, 0666);
+
+    char *str1[1024];
+    while (1)
+    {
+        fd1 = open(myfifo,O_RDONLY);
+        if (read(fd1, str1, sizeof(str1)) < 0) {
+            source = str1[0];
+            destiny = str1[1];
+            for (i=2;i<sizeof(str1)-2;i++) {
+            if (strcmp(str1[i],"bcompress")) {
+                execl("bcompress.o",NULL); //não faço ideia disto
+            }
+            else if (strcmp(str1[i],"bdecompress")) {
+
+            }
+            else if (strcmp(str1[i],"decrypt")) {
+
+            }
+            else if (strcmp(str1[i],"encrypt")) {
+
+            }
+            else if (strcmp(str1[i],"gcompress")) {
+
+            }
+            else if (strcmp(str1[i],"gdecompress")) {
+
+            }
+            else if (strcmp(str1[i],"nop")) {
+
+            }
+            }
+        }
+
+
+        close(fd1);
+        
     }
-    close(fd_log);
-    close(fd_npipe);
-
     return 0;
 }
